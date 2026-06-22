579b8645bcde80877232cf05d19fc5de7a137d27
kimhyein0214-dot <kimhyein0214@gmail.com>
2026-06-22 18:08:31 +0900

    Show Sellpia order total in inspection header

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'picking-system-0622-sellpia-amount-v1';
+const CACHE_NAME = 'picking-system-0622-sellpia-amount-v2';
 const APP_ASSETS = [
   './',
   './index.html',
-  './picking-system_0519.css?v=0622-sellpia-amount-v1',
+  './picking-system_0519.css?v=0622-sellpia-amount-v2',
   './manifest.webmanifest',
   './pwa-icon.svg'
 ];
