f403739dbc6d0edb278e8e2427eca7e468533b76
kimhyein0214-dot <kimhyein0214@gmail.com>
2026-06-25 21:20:29 +0900

    Migrate product images to System_v1 storage

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'picking-system-0625-product-code-image-v2';
+const CACHE_NAME = 'picking-system-0625-storage-image-v1';
 const APP_ASSETS = [
   './',
   './index.html',
-  './picking-system_0519.css?v=0625-product-code-image-v2',
+  './picking-system_0519.css?v=0625-storage-image-v1',
   './manifest.webmanifest',
   './pwa-icon.svg'
 ];
