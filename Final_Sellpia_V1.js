fe434d451ea1a70cddd6c5b51e947ff9d29e6c94
kimhyein0214-dot <kimhyein0214@gmail.com>
2026-06-25 15:08:26 +0900

    Use Sellpia grid selection for delivery hold

--- Final_Sellpia_V1.js
+++ Final_Sellpia_V1.js
@@ -1,6 +1,6 @@
 (async function FinalSellpiaV1(){
   const BASE_URL = 'https://kimhyein0214-dot.github.io/FULL_System/';
-  const VERSION = '0625-memo2-itemno-alias-v1';
+  const VERSION = '0625-hold-grid-selectall-v1';
   const isOrderSearch = /\/order_search\.html/i.test(location.pathname);
   const isSellpia = /(^|\.)sellpia\.com$/i.test(location.hostname) || /(^|\.)curiouswiz\.sellpia\.com$/i.test(location.hostname);
 
