2f4caf9c0d6ad87835f3bf60ef4f6d3e28769861
kimhyein0214-dot <kimhyein0214@gmail.com>
2026-06-26 18:44:45 +0900

    Fix inspection conflict target

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'picking-system-0626-pick-panel-v18';
+const CACHE_NAME = 'picking-system-0626-pick-panel-v19';
 const APP_ASSETS = [
   './',
   './index.html',
-  './picking-system_0519.css?v=0626-pick-panel-v18',
+  './picking-system_0519.css?v=0626-pick-panel-v19',
   './manifest.webmanifest',
   './pwa-icon.svg'
 ];
