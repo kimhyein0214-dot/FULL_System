d85ac7fc0d25f8933eeb99686f7f51431f38c4f5
kimhyein0214-dot <kimhyein0214@gmail.com>
2026-06-26 15:07:07 +0900

    Fix picking item selection and portrait controls

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'picking-system-0626-pick-panel-v8';
+const CACHE_NAME = 'picking-system-0626-pick-panel-v9';
 const APP_ASSETS = [
   './',
   './index.html',
-  './picking-system_0519.css?v=0626-pick-panel-v8',
+  './picking-system_0519.css?v=0626-pick-panel-v9',
   './manifest.webmanifest',
   './pwa-icon.svg'
 ];
