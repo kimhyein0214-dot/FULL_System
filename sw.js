c67816430ab6b9f41feeaaf17346e5ad8e47c2d7
kimhyein0214-dot <kimhyein0214@gmail.com>
2026-06-30 00:05:01 +0900

    Hotfix local picking UI for deployment

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'picking-system-0629-gold-insp-pwa-v2';
+const CACHE_NAME = 'picking-system-0630-local-ui-hotfix-v1';
 const APP_ASSETS = [
   './',
   './index.html',
-  './picking-system_0519.css?v=0629-gold-insp-pwa-v2',
+  './picking-system_0519.css?v=0630-local-ui-hotfix-v1',
   './manifest.webmanifest',
   './pwa-icon.svg'
 ];
