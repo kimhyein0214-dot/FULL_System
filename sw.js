c37adc542b82959d82411580d5cd3c67557c67ad
kimhyein0214-dot <kimhyein0214@gmail.com>
2026-06-30 00:39:26 +0900

    Revert "Stabilize shortage work date filtering"
    This reverts commit b406daf98c42caf3d96e27cf3a49cc178d00197e.

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'picking-system-0630-shortage-workdate-v1';
+const CACHE_NAME = 'picking-system-0630-local-ui-hotfix-v1';
 const APP_ASSETS = [
   './',
   './index.html',
-  './picking-system_0519.css?v=0630-shortage-workdate-v1',
+  './picking-system_0519.css?v=0630-local-ui-hotfix-v1',
   './manifest.webmanifest',
   './pwa-icon.svg'
 ];
