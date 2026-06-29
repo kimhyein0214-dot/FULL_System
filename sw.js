9d803f70ee2ed1534963c8c8dfbc9de9528eec9b
kimhyein0214-dot <kimhyein0214@gmail.com>
2026-06-29 11:50:25 +0900

    Fix shortage spread and restore sort toggle

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'picking-system-0626-pick-panel-v20';
+const CACHE_NAME = 'picking-system-0629-sort-toggle-v21';
 const APP_ASSETS = [
   './',
   './index.html',
-  './picking-system_0519.css?v=0626-pick-panel-v20',
+  './picking-system_0519.css?v=0629-sort-toggle-v21',
   './manifest.webmanifest',
   './pwa-icon.svg'
 ];
