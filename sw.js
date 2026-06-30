e319c63ccae168e4f4ef6d56a16af675a6bbc979
kimhyein0214-dot <kimhyein0214@gmail.com>
2026-06-30 15:07:44 +0900

    Stabilize shortage inspection workflow

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'picking-system-0630-misong-memo-v1';
+const CACHE_NAME = 'picking-system-0630-shortage-state-v1';
 const APP_ASSETS = [
   './',
   './index.html',
-  './picking-system_0519.css?v=0630-misong-memo-v1',
+  './picking-system_0519.css?v=0630-shortage-state-v1',
   './manifest.webmanifest',
   './pwa-icon.svg'
 ];
