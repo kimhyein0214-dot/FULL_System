d2c03fb0f091fdbcbeed5cf785b126dffa612c62
kimhyein0214-dot <kimhyein0214@gmail.com>
2026-06-30 10:57:45 +0900

    Add order memo sync controls

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'picking-system-0630-gold-label-all-misong-v1';
+const CACHE_NAME = 'picking-system-0630-order-memo-watch-v1';
 const APP_ASSETS = [
   './',
   './index.html',
-  './picking-system_0519.css?v=0630-gold-label-all-misong-v1',
+  './picking-system_0519.css?v=0630-order-memo-watch-v1',
   './manifest.webmanifest',
   './pwa-icon.svg'
 ];
