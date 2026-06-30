8c0685bd1cec204775ae90fc2e301448d8443421
kimhyein0214-dot <kimhyein0214@gmail.com>
2026-06-30 09:56:05 +0900

    Export all gold shortage labels

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'picking-system-0630-local-ui-hotfix-v1';
+const CACHE_NAME = 'picking-system-0630-gold-label-all-misong-v1';
 const APP_ASSETS = [
   './',
   './index.html',
-  './picking-system_0519.css?v=0630-local-ui-hotfix-v1',
+  './picking-system_0519.css?v=0630-gold-label-all-misong-v1',
   './manifest.webmanifest',
   './pwa-icon.svg'
 ];
