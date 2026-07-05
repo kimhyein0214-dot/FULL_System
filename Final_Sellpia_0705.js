29f4eca06552285fc282198b07ca83c77a3d4e7c
Miro <kimhyein0214@gmail.com>
2026-07-06 00:34:05 +0900

    Separate Sellpia detail order id from system order number

--- Final_Sellpia_0705.js
+++ Final_Sellpia_0705.js
@@ -1,6 +1,6 @@
 (async function FinalSellpia0705(){
   const BASE_URL = 'https://raw.githubusercontent.com/kimhyein0214-dot/FULL_System/main/';
-  const VERSION = '20260706-memo-retry-dedupe1';
+  const VERSION = '20260706-memo-ordno-split1';
   const isOrderSearch = /\/order_search\.html/i.test(location.pathname);
   const isSellpia = /(^|\.)sellpia\.com$/i.test(location.hostname) || /(^|\.)curiouswiz\.sellpia\.com$/i.test(location.hostname);
 
